00adb9c587590f0a8ea97105d3f4bc9456740df0
Bobby Mundy <robert.l.mundy@gmail.com>
2020-12-20 19:10:23 -0500

    added state to ls

--- projects/1 - Authentication with Auth0/auth0-app-frontend/src/App.js
+++ projects/1 - Authentication with Auth0/auth0-app-frontend/src/App.js
@@ -1,15 +1,70 @@
 import './App.css';
+import { Auth0Lock } from 'auth0-lock';
+
 
 function App() {
-  const state = {
-    accessToken: null,
-    profile: 'null'
+  console.log('running app');
+  let state = JSON.parse(localStorage.getItem('state'));
+  if(!state) {
+    localStorage.setItem('state', JSON.stringify({
+      accessToken: null,
+      profile: null
+    }));
+  }
+  const lock = new Auth0Lock(
+    'mDH7Xfrtr1mGJLSp1PvQZGuhDOoBMQXz',
+    'cylex.auth0.com'
+  );
+
+  lock.on('authenticated', (authResult) => {
+    lock.getUserInfo(authResult.accessToken, (error, profile) => {
+      if (error) {
+        return;
+      }
+
+      localStorage.setItem('accessToken', authResult.accessToken);
+      localStorage.setItem('profile', JSON.stringify(profile));
+
+      state = {
+        accessToken: authResult.accessToken,
+        profile: profile
+      };
+      localStorage.setItem('state', JSON.stringify(state));
+    });
+  });
+
+  const showLogin = () => {
+    lock.show();
+  };
+
+  const showLogout = () => {
+    state = {
+      accessToken: null,
+      profile: null
+    };
+    localStorage.setItem('state', JSON.stringify({
+      accessToken: null,
+      profile: null
+    }));
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('profile');
+    lock.logout();
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        {state.profile}
+        {!state.profile ? (
+          <div>
+            <p>User is not logged in</p>
+            <button onClick={showLogin}>Login</button>
+          </div>
+        ) : (
+          <div>
+            <p>{state.profile.name} is logged in</p>
+            <button onClick={showLogout}>Logout</button>
+          </div>
+        )}
       </header>
     </div>
   );
